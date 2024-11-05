17f76e6486c7f6509822fe3abe636f2c02fe5f97
maxmatts <120714254+maxmatts@users.noreply.github.com>
2024-11-05 18:10:56 +0100

    Update Customer-logos.js

--- src/components/Customer-logos.js
+++ src/components/Customer-logos.js
@@ -4,13 +4,13 @@ import "../styles/CustomerCase.css"; // Importera CSS-filen för att styla kompo
 // Lista över logotyper som ska visas i sektionen
 const logos = [
   "/img/decorative/case-icons/bob_hund.png",
-  "img/decorative/case-icons/fatta.png",
-  "img/decorative/case-icons/images.png",
-  "img/decorative/case-icons/nedladdning.png",
-  "img/decorative/case-icons/riksgrans.png",
-  "img/decorative/case-icons/Umea_kommun_RGB.png",
-  "img/decorative/case-icons/gbg_st_rgb_sociala+medier_high.png",
-  "img/decorative/case-icons/httpssthlm.agendajamlikhet.seimage8b1209f347ef4cb84f2e1d366858fe70a529cb32.png",
+  "/img/decorative/case-icons/fatta.png",
+  "/img/decorative/case-icons/images.png",
+  "/img/decorative/case-icons/nedladdning.png",
+  "/img/decorative/case-icons/riksgrans.png",
+  "/img/decorative/case-icons/Umea_kommun_RGB.png",
+  "/img/decorative/case-icons/gbg_st_rgb_sociala+medier_high.png",
+  "/img/decorative/case-icons/httpssthlm.agendajamlikhet.seimage8b1209f347ef4cb84f2e1d366858fe70a529cb32.png",
   "/img/decorative/case-icons/pow-main-logo.svg",
 ];
 
