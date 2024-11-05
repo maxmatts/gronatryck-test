b0f57b96e5a753de67dcf9d8ff9301aee8ab16f5
maxmatts <120714254+maxmatts@users.noreply.github.com>
2024-11-05 18:19:57 +0100

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,24 +1,18 @@
 import React from "react";
-import ServiceList from '../components/ServiceList';
+import ServiceList from "../components/ServiceList";
 import "../styles/services.css";
-import Breadcrumb from "../components/Breadcrumb";
-
 
 const Services = () => {
-
-
-
-    return (
-        <div className="services-page">¨
-        
-          <div className="service-container">
-            <div className="test-heading">
-           <h2 className="subheading-1 heading-margin title"> Tjänster</h2>
-           </div>
-           <ServiceList />
-          </div>
+  return (
+    <div className="services-page">
+      ¨
+      <div className="service-container">
+        <div className="test-heading">
+          <h2 className="subheading-1 heading-margin title"> Tjänster</h2>
         </div>
-      );
-
-}
-export default Services
+        <ServiceList />
+      </div>
+    </div>
+  );
+};
+export default Services;
